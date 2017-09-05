962d5bfc1253d12c3fc461b51f36d8c3e524dfe6
neeltrivedi <neel.trivedi@technitask.com>
2017-09-05 11:21:04 -0400

    validations in models

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -1,3 +1,8 @@
 class Encounter < ApplicationRecord
+  include ActiveModel::Validations
+
   belongs_to :patient
+
+  validates :visit_number, presence: true
+	validates :admitted_at, presence: true
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,16 @@
 class Patient < ApplicationRecord
+  include ActiveModel::Validations
+
+  before_destroy :destroy_all_encounters
+
+  has_many :encounters
+
+  validates :first_name, presence: true
+  validates :middle_name, presence: true
+  validates :mrn, presence: true
+
+  private
+    def destroy_all_encounters
+      self.encounters.delete_all
+    end
 end
